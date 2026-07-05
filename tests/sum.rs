a182292af22adce737aad81296669420b2ce7df3
Ismael Moreira <ismaelgmoreirakt@gmail.com>
2026-07-05 07:40:20 -0800

    style: apply rustfmt to test suite

--- tests/sum.rs
+++ tests/sum.rs
@@ -1,15 +1,16 @@
 use proptest::prelude::*;
 
 #[test]
-fn sumTest()
-{
+fn sum_test() {
     assert_eq!(5 + 3, 8);
 }
 
 proptest! {
     #[test]
-    fn pbSumTest(left in 0..1000i32, right in 0..1000i32)
-    {
+    fn pb_sum_test(
+        left in 0..1000i32,
+        right in 0..1000i32
+    ) {
         assert_eq!(left + right, right + left);
     }
 }
